2ff73a0d6b933d4e267fd60c209d20bae2682648
yim0327 <yim020327@gmail.com>
2026-06-30 11:47:48 +0900

    feat: initial spring boot setup

---
+++ src/main/java/com/likelion/orum/OrumApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.orum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrumApplication.class, args);
+	}
+
+}
