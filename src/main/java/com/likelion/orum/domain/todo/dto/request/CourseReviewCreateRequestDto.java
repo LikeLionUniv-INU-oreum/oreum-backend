870774640e82f6eaef65431c4ec165705ff28d05
yim0327 <yim020327@gmail.com>
2026-07-13 01:54:22 +0900

    fix(todo): 코스 리뷰 등반 시기 한글 요청 처리

--- src/main/java/com/likelion/orum/domain/todo/dto/request/CourseReviewCreateRequestDto.java
+++ src/main/java/com/likelion/orum/domain/todo/dto/request/CourseReviewCreateRequestDto.java
@@ -1,8 +1,6 @@
 package com.likelion.orum.domain.todo.dto.request;
 
-import com.likelion.orum.domain.review.enums.Grade;
 import com.likelion.orum.domain.review.enums.RecommendedGrade;
-import com.likelion.orum.domain.term.enums.TermType;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 
@@ -14,14 +12,11 @@ public record CourseReviewCreateRequestDto(
         @DecimalMax(value = "5.0", message = "만족도는 5점 이하여야 합니다.")
         Double rating,
 
-        @NotNull(message = "등반 학년을 선택해주세요.")
-        Grade ascentGrade,
-
-        @NotNull(message = "등반 학기를 선택해주세요.")
-        TermType ascentSemester,
+        @NotBlank(message = "등반 시기를 선택해주세요.")
+        String ascentPeriod,
 
         @NotEmpty(message = "추천 시기를 선택해주세요.")
-        List<@NotNull(message = "추천 시기 값이 올바르지 않습니다.")RecommendedGrade> recommendedGrades,
+        List<@NotNull(message = "추천 시기 값이 올바르지 않습니다.") RecommendedGrade> recommendedGrades,
 
         @NotBlank(message = "소요 기간을 입력해주세요.")
         String duration,
